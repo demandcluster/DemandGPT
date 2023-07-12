820fb5681302e86578cac98ddaa745ccd4e197ce
ron dlite <rondlite@gmail.com>
2023-07-12 05:01:25 +0000

    export

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -446,10 +446,8 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
-            <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
-            </div>
+            <div className={styles["main-title"]}>DemandGPT</div>
+            <div className={styles["sub-title"]}>https://demandcluster.com</div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
