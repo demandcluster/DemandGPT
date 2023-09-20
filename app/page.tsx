8e576ebd87ac2842eef37b6512769985e32be472
Gan-Xing <599153574@qq.com>
2023-09-20 10:01:56 +0800

    没钱宣言

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ const serverConfig = getServerSideConfig();
 export default async function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      大佬们过两天再来吧，账号没钱了，新买的出了点问题，需要处理。上班没时间弄。。
       {serverConfig?.isVercel && <Analytics />}
     </>
   );
